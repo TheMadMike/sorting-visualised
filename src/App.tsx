298e880bdbd5a10b624b72a5f806e568b188d001
TheMadMike <m.gibas2407@gmail.com>
2021-03-03 10:41:11 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Center, Flex } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex direction="column">
+      <Center>
+        Hello, world!
+      </Center>
+    </Flex>
+  );
+}
+
+export default App;
