d0bb3a52ec955969b97c538ff4d724769f09d81e
TheMadMike <m.gibas2407@gmail.com>
2021-03-06 21:43:11 +0100

    Display data chart and shuffle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Center, Flex } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Center, Flex, Button } from '@chakra-ui/react';
+
+import DataChart from './DataChart';
+
+import shuffle from './shuffle';
 
 function App() {
+  const [dataSet, setDataSet] = useState([1, 2, 3]);
+
   return (
     <Flex role="app" direction="column">
       <Center>
-        Hello, world!
+        <Box my="5vh" border="2px gray solid">
+          <DataChart dataSet={dataSet}/>
+        </Box>
+
+      </Center>
+
+      <Center>
+        <Button maxW="30vw" onClick={() => {
+            let toShuffle: number[] = [...dataSet];
+            setDataSet(shuffle(toShuffle));
+          }}>Shuffle data</Button>
       </Center>
     </Flex>
   );
